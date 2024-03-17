960370b1d502e17176e095f6db7ec7265c2ae32d
Nguyen Tran Nguyen <ntn@Nguyens-MacBook-Air.local>
2024-03-17 15:44:44 +0700

    problem 1

---
+++ rate_limit.py
@@ -0,0 +1,68 @@
+#Input:
+# 10 3
+# 2022-01-20T00:13:05Z
+# 2022-01-20T00:27:31Z
+# 2022-01-20T00:45:27Z
+# 2022-01-20T01:00:49Z
+# 2022-01-20T01:15:45Z
+# 2022-01-20T01:20:01Z
+# 2022-01-20T01:50:09Z
+# 2022-01-20T01:52:15Z
+# 2022-01-20T01:54:00Z
+# 2022-01-20T02:00:00Z
+
+
+# Output:
+# true 
+# true 
+# true 
+# false 
+# true 
+# false 
+# true 
+# true 
+# false
+# false
+
+from datetime import  datetime, timedelta
+from collections import deque
+
+def to_time(s):
+    return datetime.strptime(s, "%Y-%m-%dT%H:%M:%SZ")
+
+def rate_limit(logs, rate, request):
+    if not logs:
+        logs.append(request)
+        return True
+    
+    while logs and to_time(logs[0]) < to_time(request) - timedelta(hours=1):
+        logs.popleft()
+
+    if len(logs) < rate:
+        logs.append(request)
+        return True
+    return False
+
+def main(rate, requests):
+    logs = deque()
+    for request in requests:
+        if rate_limit(logs, rate, request):
+            print("true")
+        else:
+            print("false")
+
+rate = 3
+requests = [
+    "2022-01-20T00:13:05Z",
+    "2022-01-20T00:27:31Z",
+    "2022-01-20T00:45:27Z",
+    "2022-01-20T01:00:49Z",
+    "2022-01-20T01:15:45Z",
+    "2022-01-20T01:20:01Z",
+    "2022-01-20T01:50:09Z",
+    "2022-01-20T01:52:15Z",
+    "2022-01-20T01:54:00Z",
+    "2022-01-20T02:00:00Z"
+]
+
+main(rate, requests)
